eaf502f1aec5f9d1c846197d58b8a47b932df16e
Lakshmik22 <136762997+Lakshmik22@users.noreply.github.com>
2023-07-07 22:46:56 -0400

    Update la107149_lab_7.c

--- la107149_lab_7.c
+++ la107149_lab_7.c
@@ -2,11 +2,10 @@
 
 void bubbleSort(int arr[], int n)
 {
-   int i, j, temp, swaps[n];
+   int i, j, temp,swap=0;
    for (i = 0; i < n; i++)
-      swaps[i] = 0;
-   for (i = 0; i < n-1; i++)
    {
+       swap=0;
        for (j = 0; j < n-i-1; j++)
        {
            if (arr[j] > arr[j+1])
@@ -14,21 +13,20 @@ void bubbleSort(int arr[], int n)
               temp = arr[j];
               arr[j] = arr[j+1];
               arr[j+1] = temp;
-              swaps[j]++;
-              swaps[j+1]++;
+              swap++;
            }
        }
+      printf("\nNumber of swaps for index %d#: %d",i,swap); 
    }
-   for (i = 0; i < n; i++)
-      printf("Number of swaps needed for index %d: %d\n", i, swaps[i]);
 }
-
 int main()
 {
    int arr[] = {97,16,45,63,13,22,7,58,72};
    int n = sizeof(arr)/sizeof(arr[0]);
    bubbleSort(arr, n);
-   int i;
+   //Print the sorted array
+   printf("\nSorted Array: ");
+    int i;
    for(i= 0; i<n; i++){
    		printf("%d ", arr[i]);
    }
